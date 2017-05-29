a9ed83324434f29b767bfb2a5dff852846512d03
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-29 17:40:19 -0600

    added filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -6,6 +6,7 @@ module.exports = {
         return todos;
       }
   },
+
   getTodos: function(){
     var stringTodos = localStorage.getItem('todos');
     var todos = [];
@@ -18,5 +19,31 @@ module.exports = {
 
     return $.isArray(todos) ? todos : [];
 
+  },
+
+  filterTodos: function(todos, showCompleted, searchText){
+    var filteredTodos = todos;
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+        return searchText.length === 0 || todo.text.toLowerCase().indexOf(searchText) > -1; 
+    });
+
+    // sort todos with non-completed items first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed){
+        return -1;
+      }else if (a.completed && !b.completed){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -59,4 +59,52 @@ describe('TodoAPI', () => {
 
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'First todo',
+        completed: true
+      },{
+        id: 2,
+        text: 'Second todo',
+        completed: false
+      },{
+        id: 3,
+        text: 'Third todo',
+        completed: true
+      },
+    ];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort todos with non-completed todos first', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos when search text is valid', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'second');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos when search text is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
