84b6ae3dfea052ccc6106b06714304289005c5aa
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-06-02 16:26:33 -0600

    added store configuration, added redux files to TodoApp

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+	console.log('new state', store.getState());
+});
+
+store.dispatch(actions.addTodo('My new todo!'))
+store.dispatch(actions.setSearchText('new'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 require('style!css!sass!applicationStyles');
